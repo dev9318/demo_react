481ce44435298047d623e2eb1a2f975499419720
dev9318 <dev.moxaj@gmail.com>
2021-05-29 03:03:19 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
+import './index.css';
 
 function App() {
     const title = 'Welcome to blog';
