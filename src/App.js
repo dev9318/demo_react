d59b36b6e8bcef25d1840a0ac6efec3fd78f8c82
dev9318 <dev.moxaj@gmail.com>
2021-05-29 02:52:00 +0530

    vid 3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
     const title = 'Welcome to blog';
     const link = 'http://www.google.com';
   return (
-    <div className="App">
-	  <div className="content">
-	  <h1>{title}</h1>
+	  <div className="App">
+	  <Navbar />
+	       <div className="content">
+	  
+	          <Home />
 
-          <p>{[1,2,3,4]}</p>
-	  <p>{Math.random()*10}</p>
+                  <p>{[1,2,3,4]}</p>
+	          <p>{Math.random()*10}</p>
 
-	  <a href={link}>Google!</a>
-      </div>
-    </div>
+	          <a href={link}>Google!</a>
+                </div>
+           </div>
   );
-}// we cannot output objects like dictonaries
+}// we cannot output objects like dictonaries using {}
 
 export default App;
