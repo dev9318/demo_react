44c0edc4358b624232cff17dca108bc07f9875b9
dev9318 <dev.moxaj@gmail.com>
2021-05-29 13:15:57 +0530

    function as prop

--- src/Home.js
+++ src/Home.js
@@ -9,13 +9,17 @@ const Home = () => {
 	    {title: 'New', body: 'lopem', author: 'luigi', id:2},
 	    {title: 'New', body: 'lopem', author: 'peach', id:3},
 	]);
-
-
+    
+    const handleDelete = (id) => {
+	const newBlogs = blogs.filter(blog => blog.id !== id);
+	setBlogs(newBlogs);
+    }
+    
     
     return (
 	<div className="home">
-	    <BlogList blogs={blogs} title = "all blogs"/>
-	    <BlogList blogs={blogs.filter ((blog) => blog.author === 'peach')} title= 'Peach blog'/>
+	    <BlogList blogs={blogs} title = "all blogs" handleDelete={handleDelete}/>
+	    <BlogList blogs={blogs.filter ((blog) => blog.author === 'peach')} title= 'Peach blog' handleDelete={handleDelete}/>
 	</div>
     );
 }
