33db306b07266fdc604a3c9422bdc1583dc9d785
dev9318 <dev.moxaj@gmail.com>
2021-05-29 18:15:31 +0530

    saving changes

---
+++ src/useFetch.js
@@ -0,0 +1,28 @@
+const useFetch = () => {
+
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+    
+    useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+        if (!res.ok) { // error coming back from server
+          throw Error('could not fetch the data for that resource');
+        } 
+        return res.json();
+      })
+      .then(data => {
+        setIsPending(false);
+        setBlogs(data);
+        setError(null);
+      })
+      .catch(err => {
+        // auto catches network / connection error
+        setIsPending(false);
+        setError(err.message);
+      })
+    }, 1000);
+  }, [])
+}
