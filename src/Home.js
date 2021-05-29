71081dc81d6abc82d28a2d059552aa59829f18a3
dev9318 <dev.moxaj@gmail.com>
2021-05-29 13:19:52 +0530

    useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -15,6 +15,9 @@ const Home = () => {
 	setBlogs(newBlogs);
     }
     
+    useEffect( () => {
+	console.log('use effect ran');
+    });
     
     return (
 	<div className="home">
