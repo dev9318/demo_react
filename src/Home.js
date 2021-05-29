647cc6ba1acdbc7b6068fb7b94b7ea8501b54f74
dev9318 <dev.moxaj@gmail.com>
2021-05-29 13:05:29 +0530

    props

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
 
@@ -13,13 +14,7 @@ const Home = () => {
     
     return (
 	<div className="home">
-	    <h2>Home Page</h2>
-	    {blogs.map((blog) => (
-		    <div className='blog-preview' key = {blog.id}>
-		    <h2> {blog.title}</h2>
-		    <p> Writtten by {blog.author} </p>
-		    </div>
-	    ))}
+	    <BlogList blogs={blogs}/>
 	</div>
     );
 }
