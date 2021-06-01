7e120844bb7f516adbd4d3e33ab6095727d8ae9b
dev9318 <dev.moxaj@gmail.com>
2021-06-01 18:14:43 +0530

    subit form

--- src/Create.js
+++ src/Create.js
@@ -5,11 +5,17 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('Peach');
+
+    const handleSubmit = (e)=>{
+	e.preventDefault();
+	const blog = {title, body, author};
+	console.log(blog);
+    }
     
     return (
 	    <div className="create">
 	    <h2> Create </h2>
-	    <form>
+	    <form onSubmit={handleSubmit}>
 	    <label> Blog title: </label>
 	    <input type="text" required value={title} onChange={(e)=>setTitle(e.target.value)}/>
 	    <label> Blog body: </label>
