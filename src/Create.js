c705c1f27302f5bb2bba6cfa66010243f6b70c6e
dev9318 <dev.moxaj@gmail.com>
2021-06-01 18:21:38 +0530

    POST request

--- src/Create.js
+++ src/Create.js
@@ -5,12 +5,23 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('Peach');
-
+    const [isPending, setIsPending] = useState(false);
+    
     const handleSubmit = (e)=>{
 	e.preventDefault();
 	const blog = {title, body, author};
-	console.log(blog);
-    }
+
+	setIsPending(false);
+	
+	fetch('http://localhost:8000/blogs', {
+	    method: 'POST',
+	    headers: {"Cpntent-Type":"application/json"},
+	    body: JSON.stringify(blog),
+	}).then(()=>{
+	    console.log('New Blog');
+	    setIsPending(false);
+	})
+    };
     
     return (
 	    <div className="create">
@@ -26,7 +37,8 @@ const Create = () => {
 	    <option value="peach">peach</option>
 	    <option value="mario">mario</option>
 	    </select>
-	    <button>Add Blog</button>
+	    {!isPending && <button>Add Blog</button>}
+	{isPending && <button disabled>Adding Blog</button>}
 	    </form>
 	    </div>
     );
