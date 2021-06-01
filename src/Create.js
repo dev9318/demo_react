46633c1f6b928769740ba724f17ff01680e8d8d7
dev9318 <dev.moxaj@gmail.com>
2021-06-01 18:25:59 +0530

    history

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import {useHistory} from 'react-router-dom';
 
 const Create = () => {
 
@@ -6,6 +7,7 @@ const Create = () => {
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('Peach');
     const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
     
     const handleSubmit = (e)=>{
 	e.preventDefault();
@@ -20,6 +22,7 @@ const Create = () => {
 	}).then(()=>{
 	    console.log('New Blog');
 	    setIsPending(false);
+	    history.push('/');
 	})
     };
     
